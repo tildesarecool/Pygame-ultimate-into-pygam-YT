d4c2d7b3c0a80059f9eb904251a40b0258bfe504
Tildes <tildesare.cool@gmail.com>
2023-03-30 18:00:56 -0700

    initial commit for this video on pygame

---
+++ newone.py
@@ -0,0 +1,13 @@
+'''
+video: of about 3h 45m
+
+https://www.youtube.com/watch?v=AY9MnQ4x3zk
+
+Link to the project folder: https://github.com/clear-code-projects/UltimatePygameIntro
+
+Link to the background music: https://opengameart.org/content/5-chiptunes-action
+Link to the artwork: https://opengameart.org/content/platformer-art-pixel-edition
+
+'''
+
+import pygame
