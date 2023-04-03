83ade422c8acb2d91cdb830070e4a7ed545c86cb
Tildes <tildesare.cool@gmail.com>
2023-04-02 17:17:58 -0700

    i think i understand rectangles now

--- runner.py
+++ runner.py
@@ -29,18 +29,24 @@ pygame.display.set_caption('Runner')
 clock = pygame.time.Clock()
 test_font = pygame.font.Font('font/Pixeltype.ttf', 50)
 
-# test_surface = pygame.Surface((100, 200)) # uses width/height - i guess double parens is necessary
-
-# test_surface.fill('Red')
 
 # convert make the PNG intosome kind of native python/pygame format...or something -- 50:30 in
+# surface origin point always upper left corner
+# rectangle (for collission box) can be set to midleft/midbottom etc which adjusts the coords
+# this combination of surface/rectwill be repplaced by sprite class/something else later
+# for now "move rect instead of surface" is proper way to move element/character
 sky_surface = pygame.image.load('graphics/sky.png').convert()
 ground_surface = pygame.image.load('graphics/ground.png').convert()
 text_surface = test_font.render('My Game', False, 'Black') # text, anti-alias true/false, color
 
-snail_surface = pygame.image.load('graphics/snail/snail1.png').convert_alpha() # alpha to acknowledge transparancy of PNG image
+snail_surf = pygame.image.load('graphics/snail/snail1.png').convert_alpha() # alpha to acknowledge transparancy of PNG image
+snail_rect = snail_surf.get_rect(midbottom = (600,300))
 
-snail_x_pos = 600
+#snail_x_pos = 600  # no longer needed
+
+player_surf = pygame.image.load('graphics/player/player_walk_1.png').convert_alpha()
+player_rect = player_surf.get_rect(midbottom = (80, 300)) # effectively sets the surface and rectanble into one thing
+
 
 # setup infinite game loop
 while True:
@@ -53,22 +59,16 @@ while True:
     screen.blit(sky_surface,(0,0)) # takes surface and position
     screen.blit(ground_surface,(0,300)) # takes surface and position
     screen.blit(text_surface, (300, 50))
-# move to right is +=
-    # snail_x_pos += 1
-# move to left is -=
-#    snail_x_pos -= 1
-    snail_x_pos -= 3
 
-    if (snail_x_pos < -100):
-        snail_x_pos = 740
-    else:
-        snail_x_pos -= 3
-         
-    screen.blit(snail_surface, (snail_x_pos, 265))
+    snail_rect.x -= 4
+    if snail_rect.right <= 0: snail_rect.left = 800 # 800 is right side of screen
+    screen.blit(snail_surf, snail_rect)
+    #snail_x_pos -= 3
 
-
+    player_rect.left += 1 # as defined aboe, the rectangle around the player surface with "origin point" midbottom - this moves it via cordinates
+#    print(player_rect.left) # useful for output of cords
+    screen.blit(player_surf, player_rect) # now display both via biltter
 
     # update everything
     pygame.display.update() # connected back to that screen variable
     clock.tick(60) # 60 fps
-
